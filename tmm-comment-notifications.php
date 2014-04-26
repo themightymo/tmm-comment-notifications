c4ae6b77baecbeadb66243dcf2c5731ff685513d
Toby <themightymo@users.noreply.github.com>
2014-04-26 16:46:24 -0500

    Updated all sorts of email content

--- tmm-comment-notifications.php
+++ tmm-comment-notifications.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.themightymo.com
 Description: Send comment notifications to specific users for EVERY comment that is posted on a site or sub-site. Uses Advanced Custom Fields. Multi-site ready!
 Author: Sherwin Calims
 Author URI: http://www.themightymo.com
-Version: 0.1
+Version: 0.2
 Text Domain: tmm-comment-notifications
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 Initially Created By: Sherwin Calims
@@ -31,9 +31,10 @@ function notify_author_of_reply($comment_id, $approved){
   if($approved){
     $comment = get_comment($comment_id);
 	$post = get_post($comment->comment_post_ID);
-	$subject = $current_blog_details->blogname . " - New Comment at : " .$post->post_title;
-	$notify_message = __('Comment : ') . "\r\n" . $comment->comment_content . "\r\n\r\n";
-	$notify_message .= '<a href="'.get_comment_link( $comment_id ).'">Click to reply the comment.</a>' . "\r\n";
+	$comment_author = ($comment->comment_author);
+	$subject = $current_blog_details->blogname . " - New comment from " . $comment_author . " on \"" .$post->post_title . "\"";
+	$notify_message = "On " . get_the_date("l, F jS, Y") . " at " . get_the_time() . ", " . $comment->comment_author . " wrote: \r\n\r\n" . $comment->comment_content . "\r\n\r\n";
+	$notify_message .= '<a href="'.get_comment_link( $comment_id ).'">Click here to reply.</a>' . "\r\n";
 	
 	wp_mail($emails_to_be_notified, $subject, $notify_message);
   }
